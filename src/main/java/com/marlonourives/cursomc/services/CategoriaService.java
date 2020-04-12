6aba39312eddff6ff8b18858392545c49aac446e
Marlon Ourives <marloncicero_araujo@hotmail.com>
2020-04-11 21:48:04 -0400

    Metodo Delete da Categoria

--- src/main/java/com/marlonourives/cursomc/services/CategoriaService.java
+++ src/main/java/com/marlonourives/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.marlonourives.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.marlonourives.cursomc.domain.Categoria;
 import com.marlonourives.cursomc.repositories.CategoriaRepository;
+import com.marlonourives.cursomc.services.exceptions.DataIntegrityException;
 import com.marlonourives.cursomc.services.exceptions.ObjectNotFoundException;
 
 import java.util.Optional;
@@ -29,5 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produto");
+		}
 	}
 }
